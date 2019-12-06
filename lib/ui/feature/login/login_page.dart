872b54339cfb378ca35ecd91af99375770c60ab1
filippoveggo <filippoveggo@gmail.com>
2019-12-06 13:01:00 +0100

    added login page

--- lib/ui/feature/login/login_page.dart
+++ lib/ui/feature/login/login_page.dart
@@ -10,8 +10,53 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hi man!'),
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.fromLTRB(90, 0, 0, 0),
+        child: Column(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 197, 0, 0),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text("Welcome,"),
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text("Login with "),
+                    ),
+                    Container(
+                      child: Text("Classeviva",
+                          style: TextStyle(color: Colors.red[300])),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Username',
+                        contentPadding: EdgeInsetsGeometry.lerp(const EdgeInsetsDirectional.only(end : 0.0),null, 10.0)
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
