0f0b0b01d775ffcea4b56e961f65af16356ec262
Riccardo <riccardocalligaro@gmail.com>
2019-12-08 15:06:45 +0100

    changed app routes, added main, login, home

--- lib/component/routes.dart
+++ lib/component/routes.dart
@@ -1,10 +1,15 @@
 import 'package:registro_elettronico/ui/feature/home/home_page.dart';
 import 'package:registro_elettronico/ui/feature/login/login_page.dart';
-import 'package:registro_elettronico/ui/feature/pages.dart';
 import 'package:registro_elettronico/ui/feature/splash_screen/splasch_screen.dart';
 
 class Routes {
   static const MAIN = '/';
+  static const LOGIN = '/login';
+  static const HOME = '/home';
 
-  static var routes = {MAIN: (ctx) => SplashScreen()};
+  static var routes = {
+    MAIN: (ctx) => SplashScreen(),
+    LOGIN: (ctx) => LoginPage(),
+    HOME: (ctx) => HomePage()
+  };
 }
