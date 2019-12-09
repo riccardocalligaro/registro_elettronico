037a691db1190e00ff94b4daea5abd3c764f3ff9
Riccardo <riccardocalligaro@gmail.com>
2019-12-09 23:18:06 +0100

    added profile utils to obtain a random string and an ident

---
+++ lib/utils/profile_utils.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'dart:math';
+
+class ProfileUtils {
+  static final Random _random = Random.secure();
+
+  /// Generates a random secure string for the key in the database
+  static String createCryptoRandomString([int length = 32]) {
+    var values = List<int>.generate(length, (i) => _random.nextInt(256));
+    return base64Url.encode(values);
+  }
+
+  /// Classeviva in the request requires and id, that you can
+  /// obtain by removing the letters and keeping onyl the numbers
+  ///  S6102171X -> 6102171
+  static String getIdFromIdent(String ident) {
+    return ident.replaceAll(RegExp('[A-Za-z]'), '');
+  }
+}
