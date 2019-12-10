682ee46efbdf09accb2fa2da394a9099feed8b9a
Riccardo <riccardocalligaro@gmail.com>
2019-12-10 19:34:35 +0100

    added functions to reduce subject title  and generate a color from the position

---
+++ lib/utils/global_utils.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class GlobalUtils {
+  static String reduceSubjectTitle(String name) {
+    //TODO: try to reduce name by creating a switch with abbreviations
+    String reducedName = name.substring(0, 20);
+    reducedName += "...";
+    return reducedName;
+  }
+
+  static String reduceLessonArgument(String argument) {
+    String reducedName = argument.substring(0, 30);
+    reducedName += "...";
+    return reducedName;
+  }
+
+  static Color generateRandomColor() {
+    return Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+        .withOpacity(1.0);
+  }
+
+  static Color getColorFromPosition(int position) {
+    // TODO: generate more colors, 10 - 15
+    switch (position) {
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.indigo;
+        break;
+      case 3:
+        return Colors.green;
+        break;
+      case 4:
+        return Colors.amber;
+        break;
+      case 5:
+        return Colors.deepOrange;
+        break;
+      case 6:
+        return Colors.indigo;
+        break;
+      default:
+        return Colors.red;
+    }
+  }
+}
