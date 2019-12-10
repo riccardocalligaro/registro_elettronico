fa9b3ed4f90de47226ef92b56ce7b6f927ac6b1a
Riccardo Calligaro <riccardocalligaro@gmail.com>
2019-12-10 11:14:08 +0100

    cleaned code

--- lib/data/repository/mapper/lesson_mapper.dart
+++ lib/data/repository/mapper/lesson_mapper.dart
@@ -1,19 +1,17 @@
 import 'package:registro_elettronico/data/db/moor_database.dart' as db;
-import 'package:registro_elettronico/domain/entity/lessons_response.dart'
-    as entity;
 
-/// "evtId": 7096829,
-///           "evtDate": "2019-12-09",
-///           "evtCode": "LSS0",
-///           "evtHPos": 1,
-///           "evtDuration": 1,
-///           "classDesc": "4IA INFORMATICA",
-///           "authorName": "TRENTO PAOLO",
-///           "subjectId": 5,
-///           "subjectCode": "SOST",
-///           "subjectDesc": "SOSTEGNO",
-///           "lessonType": "Compresenza",
-///           "lessonArg": ""
+///"evtId": 7096829,
+///"evtDate": "2019-12-09",
+///"evtCode": "LSS0",
+///"evtHPos": 1,
+///"evtDuration": 1,
+///"classDesc": "4IA INFORMATICA",
+///"authorName": "TRENTO PAOLO",
+///"subjectId": 5,
+///"subjectCode": "SOST",
+///"subjectDesc": "SOSTEGNO",
+///"lessonType": "Compresenza",
+///"lessonArg": ""
 
 class LessonMapper {
   const LessonMapper();
