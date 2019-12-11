2c3004bedbabe5401b0667949c734b48f85f1f7b
Riccardo <riccardocalligaro@gmail.com>
2019-12-11 17:17:14 +0100

    added states of subject

--- lib/ui/bloc/subjects/subjects_state.dart
+++ lib/ui/bloc/subjects/subjects_state.dart
@@ -2,5 +2,17 @@ import 'package:meta/meta.dart';
 
 @immutable
 abstract class SubjectsState {}
-  
-class InitialSubjectsState extends SubjectsState {}
+
+class SubjectsInitialState extends SubjectsState {}
+
+class SubjectsNotLoaded extends SubjectsState {}
+
+class SubjectsLoading extends SubjectsState {}
+
+class SubjectsError extends SubjectsState {
+  final String error;
+
+  SubjectsError(this.error);
+}
+
+class SubjectsLoaded extends SubjectsState {}
