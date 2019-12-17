78ba2ac01d1b0876aecc36451fea1cfe01708307
Filippo <filippoveggo@gmail.com>
2019-12-17 23:38:16 +0100

    added agenda event design

--- lib/ui/feature/agenda/agenda_page.dart
+++ lib/ui/feature/agenda/agenda_page.dart
@@ -163,18 +163,17 @@ class _AgendaPageState extends State<AgendaPage> with TickerProviderStateMixin {
   Widget _buildEventList() {
     return ListView(
       children: _selectedEvents
-          .map((event) => Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 0.8),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+          .map(
+            (event) => Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+              child: Card(
                 child: ListTile(
-                  title: Text(event.toString()),
-                  onTap: () => print('$event tapped!'),
+                  title: Text("Italiano"),
+                  subtitle: Text(event.toString()),
                 ),
-              ))
+              ),
+            ),
+          )
           .toList(),
     );
   }
