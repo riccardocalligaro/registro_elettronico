e0ad5a6f149ae656711f3b0fe870df9e38cde45f
Riccardo Calligaro <riccardocalligaro@gmail.com>
2020-02-18 23:51:26 +0100

    Added timetable service and provider

---
+++ android/app/src/main/java/com/riccardocalligaro/registro_elettronico/widgets/TimetableService.kt
@@ -0,0 +1,4 @@
+package com.riccardocalligaro.registro_elettronico.widgets
+
+class TimetableService {
+}
--- android/app/src/main/java/com/riccardocalligaro/registro_elettronico/widgets/TimetableWidget.kt
+++ android/app/src/main/java/com/riccardocalligaro/registro_elettronico/widgets/TimetableWidget.kt
@@ -3,6 +3,8 @@ package com.riccardocalligaro.registro_elettronico
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.RemoteViews
 
 /**
@@ -29,7 +31,24 @@ internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManage
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.timetable_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    //views.setTextViewText(R.id.appwidget_text, widgetText)
+
+
+
+//    val listView = R.id.timetable_list
+
+    val words = listOf("Test1", "Test2")
+
+
+    val listItems = arrayOfNulls<String>(words.size)
+
+    for (i in words.indices) {
+        val word = words[i]
+        listItems[i] = words[i]
+    }
+
+
+    val adapter = ArrayAdapter(context, android.R.layout.simple_dropdown_item_1line, listItems)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
