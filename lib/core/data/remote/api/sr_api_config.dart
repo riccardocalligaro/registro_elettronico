cc844008116d77834e6e8cf75888c6fedc3fdf73
Riccardo Calligaro <riccardocalligaro@gmail.com>
2023-03-25 16:58:42 +0700

    fix(api-key): change api key and make build for target 31

--- lib/core/data/remote/api/sr_api_config.dart
+++ lib/core/data/remote/api/sr_api_config.dart
@@ -1,7 +1,7 @@
 abstract class SRApiConfig {
   static const String baseApiUrl = 'https://web.spaggiari.eu/rest/v1';
-  static const String baseUserAgent = 'zorro/1.0';
-  static const String apiKey = '+zorro+';
+  static const String baseUserAgent = 'CVVS/std/4.2.3';
+  static const String apiKey = 'Tg1NWEwNGIgIC0K';
   static const String headeraApiKey = 'Z-Dev-Apikey';
   static const String headerToken = 'Z-Auth-Token';
   static const String loginPath = '/auth/login';
